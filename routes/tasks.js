16b6123cd42293a4d7aed0d02177602c4388d0ec
BahkoHatake <cbranko01@gmail.com>
2024-03-14 01:05:14 +0100

    Add functionalities for routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,8 +2,13 @@ const Task = require("../models/task");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Get all tasks");
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 });
 
 router.post("/", async (req, res) => {
@@ -11,18 +16,45 @@ router.post("/", async (req, res) => {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ msg: error });
   }
 });
-router.get("/:id", (req, res) => {
-  res.json({ id: req.params.id });
+router.get("/:id", async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    if (!task) {
+      return res.status(404).json({ msg: "Could not find task with that id " });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 });
-router.patch("/:id", (req, res) => {
-  res.json({ id: req.params.id });
+router.patch("/:id", async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: "Could not find task with that id " });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 });
-router.delete("/:id", (req, res) => {
-  res.json({ id: req.params.id });
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id });
+    if (!task) {
+      return res.status(404).json({ msg: "Could not find task with that id " });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 });
 
 module.exports = router;
